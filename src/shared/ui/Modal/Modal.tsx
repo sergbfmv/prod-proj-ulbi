783d3df6a7fd882aecf18507f76fbc9fd76f4e85
Sergey <sergbfmv@mail.ru>
2025-01-23 15:44:22 +0300

    feat: add portal, modal, change header

---
+++ src/shared/ui/Modal/Modal.tsx
@@ -0,0 +1,74 @@
+import { classNames } from 'shared/lib/classNames/classNames';
+import React, {
+  ReactNode, useCallback, useEffect, useRef, useState,
+} from 'react';
+import { Portal } from 'shared/ui/Portal/Portal';
+import { useTheme } from 'app/providers/ThemeProvider';
+import s from './Modal.module.scss';
+
+interface ModalProps {
+    className?: string;
+    children?: ReactNode;
+    isOpen?: boolean;
+    onClose?: () => void;
+}
+
+export const Modal = ({
+  className,
+  children,
+  isOpen,
+  onClose,
+}: ModalProps) => {
+  const ANIMATION_DELAY = 300;
+  const [isClosing, setIsClosing] = useState(false);
+  const timerRef = useRef<ReturnType<typeof setTimeout>>();
+  const { theme } = useTheme();
+
+  const mods: Record<string, boolean> = {
+    [s.opened]: isOpen,
+    [s.isClosing]: isClosing,
+    [s[theme]]: true,
+  };
+
+  const closeHandler = useCallback(() => {
+    if (onClose) {
+      setIsClosing(true);
+      timerRef.current = setTimeout(() => {
+        onClose();
+        setIsClosing(false);
+      }, ANIMATION_DELAY);
+    }
+  }, [onClose]);
+
+  const onContentClick = (e: React.MouseEvent) => {
+    e.stopPropagation();
+  };
+
+  const onKeyDown = useCallback((e: KeyboardEvent) => {
+    if (e.key === 'Escape') {
+      closeHandler();
+    }
+  }, [closeHandler]);
+
+  useEffect(() => {
+    if (isOpen) {
+      window.addEventListener('keydown', onKeyDown);
+    }
+    return () => {
+      clearTimeout(timerRef.current);
+      window.removeEventListener('keydown', onKeyDown);
+    };
+  }, [isOpen, onKeyDown]);
+
+  return (
+    <Portal>
+      <div className={classNames(s.Modal, mods, [className])}>
+        <div className={s.overlay} onClick={closeHandler}>
+          <div className={s.content} onClick={onContentClick}>
+            {children}
+          </div>
+        </div>
+      </div>
+    </Portal>
+  );
+};
--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,14 +1,32 @@
 import { classNames } from 'shared/lib/classNames/classNames';
-import cls from './Navbar.module.scss';
+import { Modal } from 'shared/ui/Modal/Modal';
+import React, { useCallback, useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import { Button, ButtonTheme } from 'shared/ui/Button/Button';
+import s from './Navbar.module.scss';
 
 interface NavbarProps {
     className?: string;
 }
 
-export const Navbar = ({ className }: NavbarProps) => (
-  <div className={classNames(cls.Navbar, {}, [className])}>
-    <div className={cls.links}>
-      /
+export const Navbar = ({ className }: NavbarProps) => {
+  const { t } = useTranslation();
+  const [isAuthModal, setIsAuthModal] = useState(false);
+
+  const onToggleModal = useCallback(() => {
+    setIsAuthModal((prev) => !prev);
+  }, []);
+
+  return (
+    <div className={classNames(s.Navbar, {}, [className])}>
+      <Button theme={ButtonTheme.CLEAR_INVERTED} className={s.links} onClick={onToggleModal}>
+        {t('Войти')}
+      </Button>
+      {/* eslint-disable-next-line i18next/no-literal-string */}
+      <Modal isOpen={isAuthModal} onClose={onToggleModal}>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem, consequuntur
+        delectus dignissimos inventore itaque minima minus odit perferendis quae rerum!
+      </Modal>
     </div>
-  </div>
-);
+  );
+};
